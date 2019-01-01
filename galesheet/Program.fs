14e10edaf62ac1702ff64cd067f29b751db3b1db
TurboHz <turbohz@gmail.com>
2019-01-01 21:24:45 +0100

    Genesis
    Implements basic .gal file loading of an included example file.

---
+++ galesheet/Program.fs
@@ -0,0 +1,11 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open GraphicsGaleWrapper
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    let go = new GaleObject("assets/bat_fly.gal")
+    printfn "Loaded animation %ix%i" go.Width go.Height
+    0 // return an integer exit code
