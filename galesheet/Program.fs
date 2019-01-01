91a518369faa839ab0818c6fc475a1190a4a014f
TurboHz <turbohz@gmail.com>
2019-01-01 22:44:47 +0100

    Takes a file path and displays animation info

--- galesheet/Program.fs
+++ galesheet/Program.fs
@@ -4,19 +4,26 @@ open System
 open GraphicsGaleWrapper
 open System.IO
 
+let showInfo (go:GaleObject) =
+    let width = go.Width
+    let height = go.Height
+    let totalFrames = go.FrameCount
+    printfn "Loaded %ix%i animation with %i frames" width height totalFrames
+    ()
+
 [<EntryPoint>]
 let main argv =
-    
+
     printfn "GalSheet v0.1"
 
-    try 
-        let path = 
+    try
+        let path =
             match (List.ofArray argv) with
                 | path::_ -> path
-                | _ -> failwith "Invalid path"
+                | _ -> failwith "A path is required"
 
         let go = new GaleObject(path)
-        printfn "Loaded animation %ix%i" go.Width go.Height
+        showInfo go
         0
     with
         | e ->  printfn "Error: %s" e.Message; 1
